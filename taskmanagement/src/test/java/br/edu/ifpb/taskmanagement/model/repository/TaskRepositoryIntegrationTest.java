b613b43645a0c150f8f25614646bf0939e0bf1c6
Emerson Andrey Fausto dos Santos <202225020017@ad.ifpb.edu.br>
2025-02-27 19:50:06 -0300

    Add: testes de integracao do TaskRepository

--- taskmanagement/src/test/java/br/edu/ifpb/taskmanagement/model/repository/TaskRepositoryIntegrationTest.java
+++ taskmanagement/src/test/java/br/edu/ifpb/taskmanagement/model/repository/TaskRepositoryIntegrationTest.java
@@ -3,6 +3,10 @@ package br.edu.ifpb.taskmanagement.model.repository;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +33,47 @@ public class TaskRepositoryIntegrationTest {
         assertNotNull(savedTask.getId());
         assertEquals("New Task", savedTask.getTitle());
         assertFalse(savedTask.isCompleted());
+    }
+
+    @Test
+    void givenTaskID_whenFind_thenFindTaskInDatabase(){
+
+        TaskEntity task = new TaskEntity(null, "New Task" ,false);
+
+        TaskEntity savedTask = taskRepository.save(task);
+        TaskEntity foundTask = taskRepository.findById(savedTask.getId()).get();
+        
+        assertNotNull(foundTask);
+        assertEquals(savedTask.getId(), foundTask.getId());
+    }
+
+    @Test
+    void givenTask_whenFindAll_thenFindAllTasksInDatabase(){
+
+        TaskEntity task1 = new TaskEntity(null, "New Task 1" ,false);
+        TaskEntity task2 = new TaskEntity(null, "New Task 2" ,false);
+
+        TaskEntity savedTask1 = taskRepository.save(task1);
+        TaskEntity savedTask2 = taskRepository.save(task2);
+        List<TaskEntity> foundTask = taskRepository.findAll();
+        
+        assertNotNull(foundTask);
+        assertNotNull(foundTask.get(0));
+        assertNotNull(foundTask.get(1));
+        assertEquals(savedTask1, foundTask.get(0));
+        assertEquals(savedTask2, foundTask.get(1));
+    }
+
+    @Test
+    void givenTask_whenDelete_thenDeleteTaskInDatabase(){
+
+        TaskEntity task = new TaskEntity(null, "New Task" ,false);
+
+        TaskEntity savedTask = taskRepository.save(task);
+        taskRepository.deleteById(savedTask.getId());
+
+        assertThrows(NoSuchElementException.class, () -> taskRepository.findById(savedTask.getId()).get());
+        
     }
     
 }
